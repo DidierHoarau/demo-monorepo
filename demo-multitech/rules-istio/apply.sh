e1ba92b76c9723002deba09d383588d4470f53aa
Didier Hoarau <didier.hoarau.dev@gmail.com>
2019-04-12 09:32:24 +0800

    Test Istio

--- demo-multitech/rules-istio/apply.sh
+++ demo-multitech/rules-istio/apply.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd ${SCRIPT_DIR}
+cd "${SCRIPT_DIR}"
 
 export ENV=dev
 
 kubectl -n ${ENV}-env get gateway | grep demo-multitech | cut -f1 -d" " | xargs kubectl -n ${ENV}-env delete gateway 
 kubectl -n ${ENV}-env get virtualservice | grep demo-multitech | cut -f1 -d" " | xargs kubectl -n ${ENV}-env delete virtualservice 
+
+export INGRESS_HOST=$(kubectl get po -l istio=ingressgateway -n istio-system -o jsonpath='{.items[0].status.hostIP}')
+export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
 
 kubectl -n ${ENV}-env apply -f gateway.yaml
