79c00ca13d150dbea92a1f215f58703b75fd7a9f
Didier Hoarau <didier.hoarau.dev@gmail.com>
2019-04-15 14:42:33 +0800

    Update IP

--- demo-multitech/demo-multitech-myip/src/app.js
+++ demo-multitech/demo-multitech-myip/src/app.js
@@ -3,11 +3,12 @@ const app = express();
 const port = 80;
 
 app.get('/', (req, res) => {
-  res.send(
-    `x-real-ip: ${req.headers['x-real-ip']} ; x-forwarded-for: ${
-      req.headers['x-forwarded-for']
-    } ; address: ${req.connection.remoteAddress} (@${process.env.HOSTNAME})`
-  );
+  res.send(JSON.stringify(req));
+  // res.send(
+  //   `x-real-ip: ${req.headers['x-real-ip']} ; x-forwarded-for: ${
+  //     req.headers['x-forwarded-for']
+  //   } ; address: ${req.connection.remoteAddress} (@${process.env.HOSTNAME})`
+  // );
 });
 
 app.listen(port, () => console.log(`App istening on port ${port}!`));
